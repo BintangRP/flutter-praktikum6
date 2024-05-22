02ad77e37b0debc5734692dae191e87a59fed17f
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2024-05-22 09:52:11 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+// Praktikum 9
+// ignore_for_file: use_super_parameters, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:praktek6/bloc/login/login_cubit.dart';
+import 'package:praktek6/bloc/register/register_cubit.dart';
+import 'firebase_options.dart';
+import 'package:praktek6/ui/splash.dart';
+import 'package:praktek6/utils/routes.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => LoginCubit()),
+        BlocProvider(create: (context) => RegisterCubit())
+      ],
+      child: MaterialApp(
+        title: "Praktikum 9",
+        debugShowCheckedModeBanner: false,
+        navigatorKey: NAV_KEY,
+        onGenerateRoute: generateRoute,
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
