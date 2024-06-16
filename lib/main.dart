2c99b3075889b7b59a2b02f85350d332e4d909c8
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2024-06-16 21:21:43 +0700

    feat: flutterfire praktikum CRUD TODO

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 // Praktikum 9
 // ignore_for_file: use_super_parameters, prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:praktek6/bloc/login/login_cubit.dart';
 import 'package:praktek6/bloc/register/register_cubit.dart';
-import 'firebase_options.dart';
+import 'package:praktek6/firebase_options.dart';
 import 'package:praktek6/ui/splash.dart';
 import 'package:praktek6/utils/routes.dart';
 
@@ -14,6 +15,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
   );
   runApp(const MyApp());
 }
